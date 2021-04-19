420ad2000f4749f74be94f79e0b5cb7b25a8b02a
Amrendra <amren1254@gmail.com>
2021-04-20 00:11:14 +0530

    POST: added post method

--- TestWebApp/main.go
+++ TestWebApp/main.go
@@ -4,8 +4,9 @@ import (
 	//"fmt"
 	"encoding/json"
 	"github.com/gorilla/mux"
-	//"math/rand"
+	"math/rand"
 	"net/http"
+	"strconv"
 )
 
 type Post struct {
@@ -20,10 +21,19 @@ func getPosts(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json");
 	json.NewEncoder(w).Encode(posts);
 }
+func createPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var post Post
+	_ = json.NewDecoder(r.Body).Decode(&post)
+	post.ID = strconv.Itoa(rand.Intn(1000000))
+	posts = append(posts, post)
+	json.NewEncoder(w).Encode(&post)
+}
 
 func main(){
 	router := mux.NewRouter();
 	posts = append(posts, Post{ID: "1", Title: "AwesomePost", Body: "Authentication and Authorization"});
 	router.HandleFunc("/posts", getPosts).Methods("GET");
+	router.HandleFunc("/posts", createPost).Methods("POST")
 	http.ListenAndServe(":5000",router);
 }
