33ceecd19ddbd634a5e9103813f9a4a1f2778bb2
Amrendra <amren1254@gmail.com>
2021-04-20 00:15:18 +0530

    PUT,DELETE,GET(with parameter): implemented CRUD

--- TestWebApp/main.go
+++ TestWebApp/main.go
@@ -29,7 +29,46 @@ func createPost(w http.ResponseWriter, r *http.Request) {
 	posts = append(posts, post)
 	json.NewEncoder(w).Encode(&post)
 }
+func getPost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range posts {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Post{})
+}
 
+func updatePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range posts {
+		if item.ID == params["id"] {
+			posts = append(posts[:index], posts[index+1:]...)
+			var post Post
+			_ = json.NewDecoder(r.Body).Decode(&post)
+			post.ID = params["id"]
+			posts = append(posts, post)
+			json.NewEncoder(w).Encode(&post)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(posts)
+}
+
+func deletePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range posts {
+		if item.ID == params["id"] {
+			posts = append(posts[:index], posts[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(posts)
+}
 func main(){
 	router := mux.NewRouter();
 	posts = append(posts, Post{ID: "1", Title: "AwesomePost", Body: "Authentication and Authorization"});
