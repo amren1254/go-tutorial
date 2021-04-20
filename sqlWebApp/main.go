9a01d83e245ab1bc6b861bdf54baaa6c65d0268a
Amrendra <amren1254@gmail.com>
2021-04-21 00:03:06 +0530

    Insert: added code for inserting values to sql db

--- sqlWebApp/main.go
+++ sqlWebApp/main.go
@@ -15,21 +15,41 @@ func main() {
 	var (
 		id int
 		name string
+		password string
 	)
-	rows, err := db.Query("select id, username from user where id = ?", 1)
+	//select 
+	rows, err := db.Query("select id, username, password from user")// where id = ?", 1)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
 	for rows.Next() {
-		err := rows.Scan(&id, &name)
+		err := rows.Scan(&id, &name, &password)
 		if err != nil {
 			log.Fatal(err)
 		}
-		log.Println(id, name)
+		log.Println(id, name, password)
 	}
 	err = rows.Err()
 	if err != nil {
 		log.Fatal(err)
 	}
+	insert
+	stmt, err := db.Prepare("INSERT INTO user(username,password) VALUES(?,?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	res, err := stmt.Exec("naren","naren@")
+	if err != nil {
+		log.Fatal(err)
+	}
+	lastId, err := res.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+	}
+	rowCnt, err := res.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("ID = %d, affected = %d\n", lastId, rowCnt)
 }
