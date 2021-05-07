39a4eeac14b8d320c829ac474465f580e04a973a
Amrendra <amren1254@gmail.com>
2021-05-07 16:11:13 +0530

    added comments

--- stacktracer/main.go
+++ stacktracer/main.go
@@ -5,9 +5,14 @@ import (
 	logger "stack/logger"
 )
 
+/*
+Purpose : to print customized echo logs
+input : string message to print
+input example : logger.Echo("Hey error in main")
+output :	FileName : packageName/FunctionName: Line number: : ErrorMessage
+Output Example:  main.go : main.main : Line:  11 -> Hey error in main
+*/
 func main() {
-	//initialize logger
-	//get Package.functionName,filename,line number for the functionName
-	logger.PrintLog(main, "Hey error in main")
+	logger.Echo("Hey error in main")
 	control.TestLogger()
 }
